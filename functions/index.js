c36f06c4fb16e14e5474149df07c05eaff03f464
LeulAria <leulsegedtariku@gmail.com>
2020-05-03 14:53:21 -1200

    firebase functions and travis ci init

---
+++ functions/index.js
@@ -0,0 +1,8 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send(`<h1 style="position:absolute;top:25%;left:50%;transform:translate(-50%,-50%);font-size:8vw;letter-spacing: 1vw;color:#444;font-weight: 200;font-family: monospace;">Some sheet...</h1>`);
+});
