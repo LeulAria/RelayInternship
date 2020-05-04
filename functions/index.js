6333bd706825a1d9e6e14740bf558fe60956c0e7
LeulAria <leulsegedtariku@gmail.com>
2020-05-03 15:22:32 -1200

    testing functions...

--- functions/index.js
+++ functions/index.js
@@ -4,5 +4,5 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send(`<h1 style="position:absolute;top:25%;left:50%;transform:translate(-50%,-50%);font-size:8vw;letter-spacing: 1vw;color:#444;font-weight: 200;font-family: monospace;">Some sheet...</h1>`);
+ response.send(`<h1 style="position:absolute;top:25%;left:50%;transform:translate(-50%,-50%);font-size:8vw;letter-spacing: 1vw;color:#444;font-weight: 200;font-family: monospace;"><small>Relay-Internships API</small></h1>`);
 });
